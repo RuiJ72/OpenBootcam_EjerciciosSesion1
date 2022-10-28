3377be8a8e7d415088ca79b1916384a475f719e0
RuiJ72 <rlagos900@gmail.com>
2022-10-28 00:01:00 -0300

    First commit

---
+++ EjerciciosSesion1/Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.WriteLine(" Mí nobre es Rui. Qual el Tuyo? :");
+Console.WriteLine("_________________________________");
+
+Console.WriteLine("--------------------Ejercícios Uno y Dos-----------------------------");
+Console.WriteLine();
+Console.WriteLine("Introduce tu nombre para saberes la fecha e hora en tu zona horaria: ");
+string name = Console.ReadLine();
+
+Console.WriteLine();
+Console.WriteLine("Tu nombre es " + name + ", y la fecha y hora del lugar donde te encuentras es el: " + DateTime.Now);
